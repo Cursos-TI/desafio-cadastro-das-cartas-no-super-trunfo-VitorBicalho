9f1d007ff99b9a60dd6a41bfd8c50eafa15af72e
VitorBicalho <VitorBicalho87@gmail.com>
2025-04-24 01:11:16 +0000

    Nova funcionalidade com if else,fazendo a comparação com apenas um atributo de cada carta, e uma pequena organização sobre as variáveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    
-    char estado;    
-    unsigned int carta1, carta2;
-    char nome_cidade1[50], nome_cidade2[50];
-    float populacao1, populacao2;
-    float area1, area2;
-    float pib1, pib2;
-    unsigned int ponto_turistico1, ponto_turistico2;
-
-    printf("Digite a letra do estado(A-H): ");
-    scanf("%c", &estado);
-
-    printf("Digite a numeração da carta 1: ");
-    scanf("%d", &carta1);
+    // Variáveis para armazenar os dados inseridos pelo usuário
+    char estado1[10], estado2[10], nome_cidade1[50], nome_cidade2[50];   
+    unsigned int carta1 = 1, carta2 = 2, ponto_turistico1, ponto_turistico2;
+    float populacao1, populacao2, area1, area2, pib1, pib2;
+    //Coleta de dados do usuário
+    printf("Digite as letras do estado 1: ");
+    scanf("%s", &estado1); //Não compreendi o erro descrito nesta linha, acredito que não influencie na funcionalidade do código
 
     printf("Digite o nome da cidade 1: ");
     scanf("%s", nome_cidade1);
@@ -33,8 +26,8 @@ int main() {
 
     printf("\n");
 
-    printf("Digite a numeração da carta 2: ");
-    scanf("%d", &carta2);
+    printf("Digite as letras do estado 2: ");
+    scanf("%s", &estado2);
 
     printf("Digite o nome da cidade 2: ");
     scanf("%s", nome_cidade2);
@@ -50,15 +43,14 @@ int main() {
 
     printf("Digite quantos pontos turísticos a cidade 2 possui: ");
     scanf("%d", &ponto_turistico2);
-
+    //Cálculos de densidade populacional,PIB Per Capita e super poder
     float densidadepop1 = populacao1 / area1, densidadepop2 = populacao2 / area2;
     float pibpercapita1 = pib1 / populacao1, pibpercapita2 = pib2 / populacao2;
     float superPoder1 = area1 + pib1 + pibpercapita1 + (1 / densidadepop1) + ponto_turistico1, superPoder2 = area2 + pib2 + pibpercapita2 + (1 / densidadepop2) + ponto_turistico2; 
 
     printf("\n");
-
-    printf("Carta %c%d\n", estado, carta1);
-    printf("Nome da cidade: %s\n", nome_cidade1);
+    //exibição dos atributos de cada carta
+    printf("Carta %d - %s(%s)\n", carta1, nome_cidade1, estado1);
     printf("População: %.3f\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.1f bilhões\n", pib1);
@@ -69,8 +61,7 @@ int main() {
 
     printf("\n");
 
-    printf("Carta %c%d:\n", estado, carta2);
-    printf("Nome da cidade: %s\n", nome_cidade2);
+    printf("Carta %d - %s(%s):\n", carta2, nome_cidade2, estado2);
     printf("População: %.3f\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.1f bilhões\n", pib2);
@@ -81,7 +72,9 @@ int main() {
 
     printf("\n");
 
-    printf("Comparação de cartas:\n");
+    printf("Comparação de cartas (Atributo: PIb Per Capita):\n");
+   /* 
+    Antiga comparação de cartas baseado em todos os atributos
     printf("População: Carta %c%d venceu? (%d)\n", estado, carta1, populacao1 > populacao2);
     printf("Área: %c%d venceu? (%d)\n", estado, carta1, area1 > area2);
     printf("PIB: %c%d venceu? (%d)\n", estado, carta1, pib1 > pib2);
@@ -89,6 +82,19 @@ int main() {
     printf("Densidade Populacional: %c%d venceu? (%d)\n", estado, carta1, densidadepop1 <= densidadepop2);
     printf("Pib per Capita: %c%d venceu? (%d)\n", estado, carta1, pibpercapita1 > pibpercapita2);
     printf("Super Poder: %c%d venceu? (%d)", estado, carta1, superPoder1 > superPoder2);
+    */
+   printf("Carta 1 - %s(%s):%.1f\n", nome_cidade1, estado1, pibpercapita1);
+   printf("Carta 2 - %s(%s):%.1f\n", nome_cidade2, estado2, pibpercapita2);
 
-    return 0;
+    if (pibpercapita1 > pibpercapita2) {
+        printf("Resultado: Carta %d (%s) Venceu!\n", carta1, nome_cidade1);
+   } else {
+        printf("Resultado: Carta %d (%s) Venceu!\n", carta2, nome_cidade2);
+   }
+   
+   
+   return 0;
+   
 }
+    
+
